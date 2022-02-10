a60e1632e40b2c2f8f109cbbd8df2636fb9b0ad5
Jalal Al-Mussawi <xidanko07@gmail.com>
2022-02-10 17:29:44 +0300

    improving methods detection

--- src/Filter.php
+++ src/Filter.php
@@ -5,43 +5,69 @@ namespace XiDanko\QueryFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionMethod;
 
 abstract class Filter
 {
     protected Builder $builder;
     protected Request $request;
+    private array $filterMethods = [];
+    private array $orderByMethods = [];
+    private array $orderByParams = [];
 
     public function __construct(Request $request)
     {
         $this->request = $request;
+        $this->analyseChildMethods();
+        $this->parseOrderByParams();
     }
-
-    abstract protected function filters(): array;
 
     public function apply(Builder $builder): Builder
     {
         $this->builder = $builder;
         $this->filter();
-        if ($this->request->filled('orderBy')) $this->sort();
+        $this->sort();
         return $this->builder;
     }
 
     private function filter()
     {
-        foreach ($this->filters() as $filter) {
-            if ($filter === 'orderBy') throw new MethodNotAllowedException('The orderBy method is reserved by the master class.');
-            if (!$this->request->has($filter)) continue;
-            $this->builder = $this->$filter($this->builder, $this->request->$filter);
+        foreach ($this->filterMethods as $method) {
+            if (!$this->request->has($method)) continue;
+            $this->builder = $this->$method($this->builder, $this->request->$method);
         }
     }
 
     private function sort()
     {
-        $orderByParams = explode(',', $this->request->orderBy);
-        foreach ($orderByParams as $param) {
-            [$name, $value] = explode(':', $param);
-            $method = Str::of($name)->studly()->prepend('orderBy')->__toString();
-            $this->builder = $this->$method($this->builder, $value);
+        foreach ($this->orderByMethods as $method) {
+            $key = Str::of($method)->after('orderBy')->camel()->__toString();
+            if (! array_key_exists($key, $this->orderByParams)) continue;
+            $this->builder = $this->$method($this->builder, $this->orderByParams[$key]);
         }
+    }
+
+    private function analyseChildMethods()
+    {
+        $reflection = new ReflectionClass($this);
+        $childMethods = array_filter($reflection->getMethods(), fn (ReflectionMethod $method) => $method->class === $reflection->name);
+        $childMethods = array_map(fn ($method) => $method->name, $childMethods);
+        $this->orderByMethods = array_filter($childMethods, fn ($method) => str_starts_with($method, 'orderBy'));
+        $this->filterMethods = array_diff($childMethods, $this->orderByMethods);
+    }
+
+    private function parseOrderByParams()
+    {
+        if (! $this->request->filled('orderBy')) return;
+        foreach ((array) $this->request->orderBy as $param) {
+            if (str_contains($param, ':')) [$name, $direction] = explode(':', $param);
+            else [$name, $direction] = [$param, 'asc'];
+            $this->orderByParams[$name] = $direction;
+        }
+    }
+
+    final function orderBy() {
+        // This method can't be used by the child class because it's part of the orderBy methods detection convention.
     }
 }
